9062f2a5b14584c0130ba742b2ecb5f3835af661
Kirk Solano <ksolano@Kirks-Air.fios-router.home>
2020-05-12 16:56:50 -0400

    Added humidity in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined);  
         } else {
             callback(undefined, 
-                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside.  It feels like ' + body.current.feelslike + ' degrees outside.');
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside.  It feels like ' + body.current.feelslike + ' degrees outside.  The humidity is ' + body.current.humidity + '.');
         }
     });
 };
